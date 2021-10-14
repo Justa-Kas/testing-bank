0c8d82a84085254d023e80d4f2d1717b42f3aa69
Kas <79019864+Justa-Kas@users.noreply.github.com>
2021-10-14 13:52:54 -0400

    testing

---
+++ BankOOP/Bank.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankOOP
+{
+    class Bank
+    {
+
+        //properties
+        private double Money;
+        public double money
+        {
+            get
+            {  return Money; }
+        }
+        private double Interest;
+
+        //constructor
+        public Bank()
+        {
+            Money = 0;
+            Interest = 0.06;
+        }
+        public Bank(double money)
+        {
+            Money = money;
+            Interest = 0.06;
+        }
+        public Bank(double money, double interest)
+        {
+            Money = money;
+            Interest = interest;
+        }
+
+        //methods
+        public void Deposit(double value)
+        {
+            Money += value;
+            CollectInterest();
+        }
+       
+        public double Withdraw(double value)
+        {
+            if (value <= Money)
+            {
+                Money -= value;
+                
+            }
+            else
+            {
+                Console.WriteLine("You don't have the funds for that transaction");
+                value = 0;
+            }
+            CollectInterest();
+            return value;
+        }
+
+        private void CollectInterest()
+        {
+            double result = Money * Interest;
+            Money += result;
+        }
+    }
+}
---
+++ BankOOP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank Xhiradryl = new Bank(10);
+            double x = Xhiradryl.money;
+            //.money only has a getter, can't set.
+            //Xhiradryl.money= x
+
+            Console.WriteLine(Xhiradryl.money);
+            Xhiradryl.Deposit(73);
+            Console.WriteLine(Xhiradryl.money);
+            double cash = Xhiradryl.Withdraw(148);
+            Console.WriteLine($"I am holding {cash} after withdrawl");
+            Console.WriteLine(Xhiradryl.money);
+        }
+    }
+}
